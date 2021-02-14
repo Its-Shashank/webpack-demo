73990f938126969892a127cfb6786ad18d0c66ab
Its-Shashank <gaurshashank014@gmail.com>
2021-02-14 16:26:43 +0530

    webpack server added

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    hello: "./server.js",
+    server: "./server.js",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
   },
 };
